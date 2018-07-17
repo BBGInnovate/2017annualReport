1ff69adb5ab7f82608b29db13c18a713498a87a7
Kirk Radish <kirk@kirkradish.com>
2018-07-17 16:21:26 +0000

    Remove Custom Chart Code, Dynamic Charts alread set with Visual Composer

--- page_financials.php
+++ page_financials.php
@@ -18,29 +18,6 @@
 					<div id="financials-copy">
 						<?php the_content();?>
 					</div>
-					<div id="financials-chart">
-					<?php
-						$has_chart = false;
-						$display_charts = new WP_Query(array(
-							'post_type' => 'chart',
-							'category_name' => 'Financials'
-						));
-						if ($display_charts -> have_posts()) {
-							$has_chart = true;
-							$chart_set = array();
-							while ($display_charts -> have_posts()) {
-								$display_charts -> the_post();
-								$chart_data = array(
-									'php_title' => get_the_title(),
-									'php_type' => get_field('chart_type'),
-									'php_labels' => get_field('chart_labels'),
-									'php_data' => get_field('chart_data')
-								);
-								array_push($chart_set, $chart_data);
-							}
-						}
-					?>
-					</div>
 				</div>
 			</div>
 		</div>
@@ -48,26 +25,5 @@
 </div>
 
 <?php endwhile; endif;?>
-
-<?php if ($has_chart) { ?>
-<script>
-	var hasChart = '<?php echo json_encode($has_chart); ?>';
-</script>
-<?php foreach ($chart_set as $chart_data) { ?>
-	<script>
-		// var jsonTitle = '<?php //echo $chart_data['php_title']; ?>';
-		// var jsonType = '<?php //echo $chart_data['php_type']; ?>';
-		// var jsonLabels = '<?php //echo $chart_data['php_labels']; ?>';
-		// var jsonData = '<?php //echo $chart_data['php_data']; ?>';
-		// console.log('test jQ: ' + jsonTitle);
-		// displayChart(hasChart, jsonTitle, jsonType, jsonLabels, jsonData);
-	</script>
-<?php
-	}
-} else {
-	$has_chart = false;
-}
-?>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/charts.js"></script>
 
 <?php get_footer(); ?>
